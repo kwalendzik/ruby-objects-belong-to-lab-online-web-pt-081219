8702baa7516d75837ea36c592e60dea0473bc444
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-29 13:24:21 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,14 @@
 class Post
- attr_accessor :name
+ attr_accessor :title, :author
  
- def initialize
-   @name = name
+ def initialize(title, author)
+   @title = title
+   @author = author
  end
  
 end
+ 
+drake = Title.new("Drake", "rap")
+hotline_bling = Song.new("Hotline Bling")
+ 
+hotline_bling.artist = drake
